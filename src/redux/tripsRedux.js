c8eec6944101380d46089f0aa6b48196449622c8
Kardianis <michal_kuszneruk@tlen.pl>
2021-04-03 11:39:35 +0200

    main

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -1,3 +1,5 @@
+import {parseOptionPrice} from '../utils/parseOptionPrice';
+
 /* SELECTORS */
 
 export const getAllTrips = ({trips}) => trips;
@@ -12,16 +14,25 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
-
+  if(filters.duration){
+    output = output.filter((trip) => trip.days >= parseInt(filters.duration.from) && trip.days <= parseInt(filters.duration.to));
+  }
   // TODO - filter by tags
-
+  if(filters.tags){
+    for(let tag of filters.tags){
+      output = output.filter((trip) => trip.tags.includes(tag));
+    }
+  }
   // TODO - sort by cost descending (most expensive goes first)
+  const descending = output.sort((a,b) => parseOptionPrice(b.cost).value - parseOptionPrice(a.cost).value);
 
-  return output;
+  return descending;
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
+  const filtered = trips.filter(
+    (trip) => trip.id == tripId
+  );
 
   // TODO - filter trips by tripId
 
@@ -30,7 +41,9 @@ export const getTripById = ({trips}, tripId) => {
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+  const filtered = trips.filter(
+    (trip) => trip.country.code == countryCode
+  );
 
   // TODO - filter trips by countryCode
 
